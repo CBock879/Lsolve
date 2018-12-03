8d66e5246fe825a8f0c96aef46c95c3d34a525c9
CBock879 <37879356+CBock879@users.noreply.github.com>
2018-12-02 20:02:20 -0500

    Functional Release
    First release where a solution is present

--- Dmatrix.py
+++ Dmatrix.py
@@ -1,11 +1,20 @@
 import numpy as np
 class dmat:
     def __init__ ( self , x , y ):
-        self.dmatrix = np.zeros((x,y,x,y))
+        self.dmatrix = np.zeros((x,y,x,y) ) 
+        self.X, self.Y = x,y
+
     def ddu(self,x,y):
+
         self.dmatrix[x][y][x][y] = -4
         self.dmatrix[x][y][x-1][y] = 1
         self.dmatrix[x][y][x][y-1] = 1
         self.dmatrix[x][y][x+1][y] = 1
         self.dmatrix[x][y][x][y+1] = 1
+
+    #spits out matrix to be solved
+    def outmatrix(self):
+        elements = self.X * self.Y
+        dm = self.dmatrix.reshape(elements,elements)
+        return( dm)
 
--- solver.py
+++ solver.py
@@ -1,12 +1,66 @@
 import numpy as np
-import Dmatrix.py as diff
-x = 20 #x = x size of rectangular region 
-y = 10 #y = y size of rectangular region
+import matplotlib.pyplot as plt
+from Dmatrix import dmat
+x = 8 #size of rectangular region 
+y= 6 #y = y size of rectangular region
 
-Region = diff.dmat(x,y)
+#A*sols = solmat
 
+# currently set up to be Prandtal stress function
+#defines region
+Region = dmat(x,y)
+
+#boundries
+solmat = np.ones((x,y))
+solmat[:,0] = 0
+solmat[:,y-1] = 0
+solmat[0,:]  = 0
+solmat[x-1,:] = 0
+
+#sets up inital matrix
 for i in range(1,x-1):
     for j in range(1,y-1):
+        Region.ddu(i,j)
 
+#qol thing
+x= x-1 
+y = y-1
 
+#boundry conditions
+for i in range(0,x+1):
 
+    #on upper and lower bounds y velocity is zero
+    #top boundry
+    Region.dmatrix[i][0][i][0] = 1
+    #Region.dmatrix[i][0][i][1] = -1
+    #bottom boundry
+    Region.dmatrix[i][y][i][y] = 1
+    #Region.dmatrix[i][y][i][y-1] = -1
+
+for i in range(0,y):
+    #left boundry 
+    Region.dmatrix[0][i][0][i] = 1
+    #Region.dmatrix[0][i][1][i] = -1
+
+    #right boundry
+    Region.dmatrix[x][i][x][i] = 1
+    #Region.dmatrix[x][i][x-1][i] = -1
+
+#gets differential matrix 
+A = Region.outmatrix()
+
+#inverts differential matrix
+A = np.linalg.inv(A)
+
+#reshapes matrix to be solvable
+solmat  = solmat.reshape((x+1)*(y+1))
+
+np.set_printoptions(threshold = np.nan)
+#solves the matrix 
+sols = np.matmul(A,solmat)
+
+#reshapes solutions and graphs
+sols = sols.reshape(x+1,y+1)
+plt.contourf(sols)
+
+plt.show()
